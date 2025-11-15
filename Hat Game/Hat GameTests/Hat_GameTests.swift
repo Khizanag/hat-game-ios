c8762239bda228d577af447f4bfe9c54f5aa9855
Giga Khizanishvili <Khizanag@gmail.com>
2025-11-15 23:10:06 +0400

    Initial project setup: Add Hat Game iOS app structure

---
+++ Hat Game/Hat GameTests/Hat_GameTests.swift
@@ -0,0 +1,16 @@
+//
+//  Hat_GameTests.swift
+//  Hat GameTests
+//
+//  Created by Giga Khizanishvili on 15.11.25.
+//
+
+import Testing
+
+struct Hat_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
