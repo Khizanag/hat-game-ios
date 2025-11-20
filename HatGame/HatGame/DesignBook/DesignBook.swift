c1acef5f813be5bac5307667a73beff9be80bcd9
Giga Khizanishvili <Khizanag@gmail.com>
2025-11-20 20:08:35 +0400

    Refactor shadow API to use cleaner syntax
    - Convert Shadow from static properties to enum cases
    - Add View extension with .shadow() method accepting Shadow enum
    - Update GameCard and SegmentedSelectionView to use .shadow(.large) syntax
    - Remove applyShadow helper method

--- HatGame/HatGame/DesignBook/DesignBook.swift
+++ HatGame/HatGame/DesignBook/DesignBook.swift
@@ -78,9 +78,26 @@ enum DesignBook {
     }
 
     enum Shadow {
-        static let small = ShadowStyle(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
-        static let medium = ShadowStyle(color: .black.opacity(0.15), radius: 8, x: 0, y: 4)
-        static let large = ShadowStyle(color: .black.opacity(0.2), radius: 16, x: 0, y: 8)
+        case small
+        case medium
+        case large
+        case accent
+        case none
+
+        var style: ShadowStyle {
+            switch self {
+            case .small:
+                return ShadowStyle(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
+            case .medium:
+                return ShadowStyle(color: .black.opacity(0.15), radius: 8, x: 0, y: 4)
+            case .large:
+                return ShadowStyle(color: .black.opacity(0.2), radius: 16, x: 0, y: 8)
+            case .accent:
+                return ShadowStyle(color: SwiftUI.Color(red: 0.3, green: 0.6, blue: 1.0).opacity(0.2), radius: 8, x: 0, y: 4)
+            case .none:
+                return ShadowStyle(color: .clear, radius: 0, x: 0, y: 0)
+            }
+        }
 
         struct ShadowStyle {
             let color: SwiftUI.Color
@@ -92,7 +109,8 @@ enum DesignBook {
 }
 
 extension View {
-    func applyShadow(_ style: DesignBook.Shadow.ShadowStyle) -> some View {
-        self.shadow(color: style.color, radius: style.radius, x: style.x, y: style.y)
+    func shadow(_ shadow: DesignBook.Shadow) -> some View {
+        let style = shadow.style
+        return self.shadow(color: style.color, radius: style.radius, x: style.x, y: style.y)
     }
 }
